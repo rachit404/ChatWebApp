4cd5d004c61ca3f39a0f236b3daa783021602b9d
rachit404 <doshirachit28@gmail.com>
2025-03-23 15:52:43 +0530

    add user model

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+const signup = (req, res) => {
+  console.log("signup route");
+};
+const login = (req, res) => {
+  console.log("login route");
+};
+const logout = (req, res) => {
+  console.log("logout route");
+};
+
+export { signup, login, logout };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "../src/lib/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
 const app = express();
-const PORT = 5001;
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
+  connectDB();
 });
