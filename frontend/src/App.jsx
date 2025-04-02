7952388f49da0d989c1a987feeac252295e0e63a
rachit404 <doshirachit28@gmail.com>
2025-04-02 23:52:51 +0530

    create settings page, themes dont work  yet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore.js";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore.js";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, []);
@@ -27,7 +28,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
