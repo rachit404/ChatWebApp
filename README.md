79cdd1befd906c16fd8a9bfef92284f2ab56079b
rachit404 <doshirachit28@gmail.com>
2025-04-03 22:10:59 +0530

    create chat store using zustand

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ChatWebApp
+
+Command to start the app on desired port (locally):
+
+```sh
+npm run dev -- --port 5174
+```
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useThemeStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/message/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+      console.log("Error in getUsers: ", error);
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+    } catch (error) {
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+}));
