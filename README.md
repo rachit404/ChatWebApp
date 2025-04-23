242c16e3c774e08851634a1b94d15ac1e1db4375
rachit404 <doshirachit28@gmail.com>
2025-04-23 15:15:14 +0530

    env in readme

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ npm run dev -- --port 5174
 cd backend
 node src/seeds/user.seed.js
 ```
+
+.env
+PORT=5001
+
+# chat_db added in URI
+
+MONGODB_URI=
+JWT_SECRET=mysecretkey
+NODE_ENV=development
+CLOUDINARY_CLOUD_NAME=
+CLOUDINARY_API_KEY=
+CLOUDINARY_SECRECT=
--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 // const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
